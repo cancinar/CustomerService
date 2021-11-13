1907d39acae271b1b3d2f316410b86bdc635841c
Can Cinar <cancinar@outlook.com>
2021-11-13 14:35:51 +0300

    validation exception added

---
+++ src/main/java/com/cinar/customerservice/base/exception/ValidationException.java
@@ -0,0 +1,9 @@
+package com.cinar.customerservice.base.exception;
+
+public class ValidationException extends RuntimeException {
+
+  public ValidationException(String msg) {
+    super(msg);
+  }
+
+}
