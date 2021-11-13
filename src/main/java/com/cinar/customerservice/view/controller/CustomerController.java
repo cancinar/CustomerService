8d8c0ac7d3c14ed5ebd4d8a6d111c42f2f33f08b
Can Cinar <cancinar@outlook.com>
2021-11-13 14:08:03 +0300

    create customer api created

--- src/main/java/com/cinar/customerservice/view/controller/CustomerController.java
+++ src/main/java/com/cinar/customerservice/view/controller/CustomerController.java
@@ -1,12 +1,16 @@
 package com.cinar.customerservice.view.controller;
 
 import com.cinar.customerservice.core.domain.CustomerDetailsDomain;
+import com.cinar.customerservice.core.usecase.CreateCustomerUseCase;
 import com.cinar.customerservice.core.usecase.FindCustomerUseCase;
+import com.cinar.customerservice.core.usecase.io.CreateCustomerUseCaseInput;
 import com.cinar.customerservice.core.usecase.io.FindCustomerUseCaseInput;
 import com.cinar.customerservice.view.dto.CustomerDetailsDto;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CustomerController {
 
   private final FindCustomerUseCase findCustomerUseCase;
+  private final CreateCustomerUseCase createCustomerUseCase;
 
   @GetMapping
   public ResponseEntity<CustomerDetailsDto> findCustomerDetails(
@@ -24,6 +29,20 @@ public class CustomerController {
 
     final CustomerDetailsDomain customerDetails = findCustomerUseCase.run(
             new FindCustomerUseCaseInput(id))
+        .getCustomerDetails();
+
+    return ResponseEntity.ok(CustomerDetailsDto.fromCustomerDetailsDomain(customerDetails));
+  }
+
+  @PostMapping
+  public ResponseEntity<CustomerDetailsDto> createCustomer(
+      @RequestBody CustomerDetailsDto customerDetailsDto) {
+
+    final CustomerDetailsDomain customerDetails = createCustomerUseCase.run(
+            new CreateCustomerUseCaseInput(customerDetailsDto.getName(),
+                customerDetailsDto.getCommercialName(), customerDetailsDto.getStoreNumber(),
+                customerDetailsDto.getCustomerNumber(),
+                customerDetailsDto.getAddress()))
         .getCustomerDetails();
 
     return ResponseEntity.ok(CustomerDetailsDto.fromCustomerDetailsDomain(customerDetails));
